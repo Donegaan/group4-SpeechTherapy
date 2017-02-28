b1deda6c20e87f764732dad0b8dbfe57b5f8d2b1
Andrew Donegan <donegaan@tcd.ie>
2017-02-28 20:38:47 +0000

    Added simple record page, button pressing, ripple animation and colour scheme.

--- group04-SpeachTherapyApp/app/src/main/java/com/example/sweng04/speachtherapyapp/Record.java
+++ group04-SpeachTherapyApp/app/src/main/java/com/example/sweng04/speachtherapyapp/Record.java
@@ -1,7 +1,9 @@
 package com.example.sweng04.speachtherapyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Record extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Record extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+        Intent record = getIntent();
+    }
+
+    public void backToHome(View view){ // Back button
+        Intent intent = new Intent(this,Homepage.class);
+        startActivity(intent);
     }
 }
