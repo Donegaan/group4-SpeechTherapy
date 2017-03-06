ec9e8e43920e9d66247189b6f3cee672c7ad2e5b
Andrew Donegan <donegaan@tcd.ie>
2017-03-06 16:49:15 +0000

    Removed 'stop recording' activity and put it into the 'record' activity.

--- group04-SpeachTherapyApp/app/src/main/java/com/example/sweng04/speachtherapyapp/Record.java
+++ group04-SpeachTherapyApp/app/src/main/java/com/example/sweng04/speachtherapyapp/Record.java
@@ -1,9 +1,12 @@
 package com.example.sweng04.speachtherapyapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Record extends AppCompatActivity {
 
@@ -19,8 +22,18 @@ public class Record extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void recording(View view){
-        Intent intent = new Intent(this,stopRecording.class);
-        startActivity(intent);
+    boolean firstPress=true;
+    public void recording(View view){ // Changes the buttons appearance in the record activity. Code to begin recording will go here.
+        if (firstPress) {
+            firstPress=false;
+            ImageView img = (ImageView) findViewById(R.id.start_rec);
+            img.setImageResource(R.drawable.stop_button);
+            Button btn = (Button) findViewById(R.id.record);
+            btn.setBackgroundColor(Color.RED);
+        }else{ // This is where the code to end the recording will be.
+            
+        }
     }
+
+
 }
