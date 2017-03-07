1dfb98702228dfffa67d7c2b53786065ba5e10a8
rmoloney96 <rmoloney@tcd.ie>
2017-03-07 15:30:05 +0000

    Add Category

--- group04-SpeachTherapyApp/app/src/main/java/com/example/sweng04/speachtherapyapp/AddCategory.java
+++ group04-SpeachTherapyApp/app/src/main/java/com/example/sweng04/speachtherapyapp/AddCategory.java
@@ -2,12 +2,46 @@ package com.example.sweng04.speachtherapyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.app.ActionBar.LayoutParams;
 
 public class AddCategory extends AppCompatActivity {
+    private LinearLayout mLayout;
+    private EditText mEditText;
+    private Button mButton
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_category);
+        mLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        mEditText = (EditText) findViewById(R.id.editText);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(onClick());
+        TextView textView = new TextView(this);
+        textView.setText("New text");
     }
+    private OnClickListener onClick() {
+        return new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mLayout.addView(createNewTextView(mEditText.getText().toString()));
+            }
+        };
+    }
+
+    private TextView createNewTextView(String text) {
+        final LayoutParams lparams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        final Button textView = new Button(this);
+        textView.setLayoutParams(lparams);
+        textView.setText(text);
+        return textView;
+    }
+
 }
